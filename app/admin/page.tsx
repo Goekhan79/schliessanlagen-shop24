1350ab8c3d8a0844b3b2276cf92cb5260c2ea721
Goekhan79 <g.handel.vermittlung@gmail.com>
2026-08-30 22:59:49 +0200

    Fix export syntax for Page function in page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 import sql from "@/lib/db";
 import Admin from "./ui";
 
-export default function async function Page(){
+export default async function Page(){
  const products = await sql `SELECT * FROM products ORDER BY id DESC` ;
  const orders = await sql `SELECT * FROM orders ORDER BY id DESC` ;
  return <Admin initialProducts={products as any[]} initialOrders={orders as any[]}/>;

dbb93c6ce3b6731c79337fda8fcc5c48c1dbf821
Goekhan79 <g.handel.vermittlung@gmail.com>
2026-08-30 23:00:08 +0200

    Change SQL query to use boolean for active status

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Shop from "./shop";
 import sql from "@/lib/db";
 
 export default async function Home() {
-  const products = await sql`SELECT * FROM products WHERE active=1 ORDER BY id`;
+  const products = await sql`SELECT * FROM products WHERE active=true ORDER BY id`;
   return <Shop initialProducts={products as any[]} />;
 }
